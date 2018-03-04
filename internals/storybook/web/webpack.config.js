c2db7126f373adc57b35d209e523955d5608c99b
Hemerson Vianna <hemerson.lourenco@gmail.com>
2018-03-04 01:44:38 -0300

    feat: update storybook config

--- internals/storybook/web/webpack.config.js
+++ internals/storybook/web/webpack.config.js
@@ -4,6 +4,7 @@ const webpackConfig = require('../../webpack/webpack.dev.config.js');
 module.exports = function (config, env) {
   const newConfig = storybookWebpackConfig(config, env);
   newConfig.module.rules = (newConfig.module.rules || []).concat(webpackConfig.module.rules);
+  newConfig.resolve = webpackConfig.resolve;
 
   return newConfig;
 };
